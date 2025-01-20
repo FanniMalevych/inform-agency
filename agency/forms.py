a36f3d7a2ad5b523274ca8038a509ee53c621502
fanni <fmalevych@zoolatech.com>
2025-01-20 16:20:09 +0200

    add search form

---
+++ agency/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+
+
+class RedactorUsernameSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username"}
+        ),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"}
+        ),
+    )
+
+
+class NewspaperTitleSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by title"}
+        ),
+    )
--- agency/views.py
+++ agency/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from agency.forms import TopicSearchForm, NewspaperTitleSearchForm, RedactorUsernameSearchForm
 from agency.models import Redactor, Topic, Newspaper
 
 
@@ -26,6 +27,21 @@ class TopicListView(generic.ListView):
     model = Topic
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TopicListView, self).get_context_data(**kwargs)
+        context["search_form"] = TopicSearchForm(
+            initial={"name": self.request.GET.get("name", "")}
+        )
+        return context
+
+    def get_queryset(self):
+        form = TopicSearchForm(self.request.GET)
+        if form.is_valid():
+            return Topic.objects.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return Topic.objects.all()
+
 
 class TopicCreateView(generic.CreateView):
     model = Topic
@@ -47,6 +63,21 @@ class TopicDeleteView(generic.DeleteView):
 class NewspaperListView(generic.ListView):
     model = Newspaper
     paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NewspaperListView, self).get_context_data(**kwargs)
+        context["search_form"] = NewspaperTitleSearchForm(
+            initial={"title": self.request.GET.get("title", "")}
+        )
+        return context
+
+    def get_queryset(self):
+        form = NewspaperTitleSearchForm(self.request.GET)
+        if form.is_valid():
+            return Newspaper.objects.filter(
+                title__icontains=form.cleaned_data["title"]
+            )
+        return Newspaper.objects.all()
 
 
 class NewspaperDetailView(generic.DetailView):
@@ -72,6 +103,21 @@ class NewspaperDeleteView(generic.DeleteView):
 class RedactorListView(generic.ListView):
     model = Redactor
     paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(RedactorListView, self).get_context_data(**kwargs)
+        context["search_form"] = RedactorUsernameSearchForm(
+            initial={"username": self.request.GET.get("username", "")}
+        )
+        return context
+
+    def get_queryset(self):
+        form = RedactorUsernameSearchForm(self.request.GET)
+        if form.is_valid():
+            return Redactor.objects.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return Redactor.objects.all()
 
 
 class RedactorDetailView(generic.DetailView):

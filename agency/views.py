219beea9f0bcdc8019248e6f0dd0ba0307bfec3b
fanni <fmalevych@zoolatech.com>
2025-01-20 11:37:50 +0200

    added index page

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Redactor, Topic, Newspaper
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+    }
+
+    return render(request, "agency/index.html", context=context)

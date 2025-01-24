a804508efb667ab27b147da68dd068fbda0585f8
fanni <fmalevych@zoolatech.com>
2025-01-24 23:27:12 +0200

    add ordering

--- agency/models.py
+++ agency/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["username"]
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
